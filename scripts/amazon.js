4d83b4ae1c967b96ce5a624873d7195f219d2224
Krish0110 <077bct038.krishala@pcampus.edu.np>
2023-11-19 21:50:28 +0545

    add to cart btn made interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach((product)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart-btn" data-product-id=${product.id}>
             Add to Cart
           </button>
         </div>`
@@ -55,4 +55,25 @@ products.forEach((product)=>{
 
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
 
-console.log(productsHTML);
+document.querySelectorAll('.js-add-to-cart-btn').forEach((button)=>{
+  button.addEventListener("click",()=>{
+    const productId=button.dataset.productId;
+    let matchedItem;
+    cart.forEach((item)=>{
+      if(productId===item.productId){
+        matchedItem=item;
+      }
+    });
+
+    if(matchedItem){
+      matchedItem.quantity+=1;
+    }
+    else{
+      cart.push({
+        productId:productId,
+        quantity:1
+      });
+    }
+    console.log(cart);
+  });
+})
